cefe5cb95dcd492d9ede7e9c9d16706c8347a125
Forrest Williams <ffwilliams2@alaska.edu>
2024-10-25 07:35:36 -0500

    finish test

--- tests/test_create_browse.py
+++ tests/test_create_browse.py
@@ -17,13 +17,12 @@ def test_normalize_image_array():
 
 def test_create_browse_array():
     vv_min, vv_max = 0.14, 0.52
-    test_vv = np.array([[0, vv_min**2, ((vv_min + vv_max) / 2)**2, vv_max**2, np.nan]])
+    test_vv = np.array([[0, vv_min**2, ((vv_min + vv_max - 0.001) / 2) ** 2, vv_max**2, np.nan]])
     vh_min, vh_max = 0.05, 0.259
-    test_vh = np.array([[0, vh_min**2, ((vh_min + vh_max) / 2)**2, vh_max**2, np.nan]])
+    test_vh = np.array([[np.nan, 0, vh_min**2, ((vh_min + vh_max - 0.001) / 2) ** 2, vh_max**2]])
     output_array = create_browse.create_browse_array(test_vv, test_vh)
     assert output_array.shape == (1, 5, 4)
-    # TODO: fix assertions
-    # assert np.array_equal(output_array[:, :, 0], np.array([[0, 0], [180, 255], [0, 0]]))
-    # assert np.array_equal(output_array[:, :, 1], np.array([[0, 0], [180, 255], [0, 0]]))
-    # assert np.array_equal(output_array[:, :, 0], output_array[:, :, 2])
-    # assert np.array_equal(output_array[:, :, 3], np.array([[0, 0], [255, 255], [0, 0]]))
+    assert np.array_equal(output_array[:, :, 0], np.array([[0, 0, 127, 255, 0]]))
+    assert np.array_equal(output_array[:, :, 1], np.array([[0, 0, 0, 127, 255]]))
+    assert np.array_equal(output_array[:, :, 2], np.array([[0, 0, 127, 255, 0]]))
+    assert np.array_equal(output_array[:, :, 3], np.array([[0, 255, 255, 255, 0]]))
