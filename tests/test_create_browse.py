416fbcc18e93a9f21a93d2b58bd2fc7d4374566d
Andrew Johnston <asjohnston@alaska.edu>
2024-10-25 11:34:46 -0800

    move sqrt out of normalize_image_array and use more specific tests

--- tests/test_create_browse.py
+++ tests/test_create_browse.py
@@ -4,22 +4,21 @@ from opera_rtc_s1_browse import create_browse
 
 
 def test_normalize_image_array():
-    input_array = np.arange(0, 3) ** 2
-    golden_array = np.array([0, 128, 255])
+    input_array = np.array([0.0, 1.0, 2.0, np.nan])
     output_array = create_browse.normalize_image_array(input_array, 0, 2)
-    assert np.array_equal(output_array, golden_array)
+    assert np.array_equal(output_array, np.array([0, 128, 255, 0]))
 
-    input_array = np.append(input_array.astype(float), np.nan)
-    golden_array = np.append(golden_array, 0)
-    output_array = create_browse.normalize_image_array(input_array, 0, 2)
-    assert np.array_equal(output_array, golden_array)
+    input_array = np.array([-10.0, -0.001, 0.0, 0.0001, 0.04, 2.0, 5.0, 9.999, 10.0, 10.001, 94.0, np.nan])
+    output = create_browse.normalize_image_array(input_array, 0.0, 10.0)
+    assert np.array_equal(output, np.array([0, 0, 0, 0, 1, 51, 128, 255, 255, 255, 255, 0]))
+
+    output = create_browse.normalize_image_array(input_array, 1.0, 9.0)
+    assert np.array_equal(output, np.array([0, 0, 0, 0, 0, 32, 128, 255, 255, 255, 255, 0]))
 
 
 def test_create_browse_array():
-    vv_min, vv_max = 0.14, 0.52
-    test_vv = np.array([[0, vv_min**2, ((vv_min + vv_max - 0.001) / 2) ** 2, vv_max**2, np.nan]])
-    vh_min, vh_max = 0.05, 0.259
-    test_vh = np.array([[np.nan, 0, vh_min**2, ((vh_min + vh_max - 0.001) / 2) ** 2, vh_max**2]])
+    test_vv = np.array([[0, 0.0196, 0.10857025, 0.2704, np.nan]])
+    test_vh = np.array([[np.nan, 0.0, 0.0025, 0.023716, 0.067081]])
     output_array = create_browse.create_browse_array(test_vv, test_vh)
     assert output_array.shape == (1, 5, 4)
     assert np.array_equal(output_array[:, :, 0], np.array([[0, 0, 127, 255, 0]]))
